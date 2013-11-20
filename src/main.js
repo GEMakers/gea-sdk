857d61593b3abd2921a76a85d15b52792bb06c0c
Christopher M. Baker <christopher.baker2@ge.com>
2013-11-20 11:41:06 -0500

    Added bus.wait() and callback to bus.send() to make request-response easy.

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@
 
 var util = require("util");
 var events = require("events");
+var stream = require("binary-stream");
 
 const BROADCAST_ADDRESS = 0xff;
+const COMMAND_VERSION = 0x01;
                 
 function extend(destination, source) {
     var result = { };
@@ -24,6 +26,16 @@ function extend(destination, source) {
     return result;
 }
 
+function matches(expected, message) {
+    for (var key in expected) {
+        if (expected[key] != message[key]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
 function Bus(configuration, bus) {
     var self = this;
     
@@ -32,16 +44,52 @@ function Bus(configuration, bus) {
     });
     
     bus.on("message", function (message) {
+        if (message.command == COMMAND_VERSION) {
+            self.emit("version", message);
+        }
+    
         self.emit("message", message);
     });
     
-    this.send = function (message) {
-        return bus.send(extend(message, {
+    this.wait = function(message, timeout, callback) {
+        var timer = setTimeout(function() {
+            timer = null;
+            callback(new Error("The timeout has been reached while waiting for the message"));
+        }, timeout);
+        
+        function handle(response) {
+            if (timer) {
+                if (matches(message, response)) {
+                    clearTimeout(timer);
+                    callback(null, response);
+                }
+                else {
+                    bus.once("message", handle);
+                }
+            }
+        }
+        
+        bus.once("message", handle);
+    };
+    
+    this.send = function (message, timeout, callback) {
+        message = extend(message, {
             source: configuration.address,
             destination: BROADCAST_ADDRESS,
             data: []
-        }));
+        });
+        
+        bus.send(message);
+        
+        if (callback) {
+            this.wait({
+                source: message.destination,
+                command: message.command
+            }, timeout, callback);
+        }
     };
+    
+    this.send({ command: COMMAND_VERSION });
 }
 
 util.inherits(Bus, events.EventEmitter);
