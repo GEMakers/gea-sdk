5805f035837518190b368ba807974c8f41a97e60
Ryan Hartlage <ryanplusplus@gmail.com>
2014-09-30 09:59:09 -0400

    Added publication acknowledgment to fix subscriptions

--- src/erd.js
+++ src/erd.js
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2014 General Electric
- *  
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *  
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * 
+ *
  */
 
 var stream = require("binary-stream");
@@ -23,17 +23,18 @@ const COMMAND_ERD_READ = 0xf0;
 const COMMAND_ERD_WRITE = 0xf1;
 const COMMAND_ERD_SUBSCRIBE = 0xf2;
 const COMMAND_ERD_PUBLISH = 0xf5;
+const COMMAND_ERD_PUBLISH_ACKNOWLEDGMENT = 0xf5;
 
 exports.plugin = function (bus, configuration, callback) {
     bus.on("message", function (message) {
         if (message.command == COMMAND_ERD_READ) {
             var reader = new stream.Reader(message.data, stream.BIG_ENDIAN);
             var count = reader.readUInt8();
-            
+
             if (message.data.length == count * 2 + 1) {
                 for (var i = 0; i < count; i++) {
                     var erd = reader.readUInt16();
-                    
+
                     function readResponse (data) {
                         if (data == undefined) {
                             bus.send({
@@ -49,18 +50,18 @@ exports.plugin = function (bus, configuration, callback) {
                             writer.writeUInt16(erd);
                             writer.writeUInt8(data.length);
                             writer.writeBytes(data);
-                        
+
                             bus.send({
                                 command: COMMAND_ERD_READ,
                                 data: writer.toArray(),
                                 source: configuration.address,
                                 destination: message.source
                             });
-                            
+
                             delete writer;
                         }
                     }
-                    
+
                     bus.emit("read", {
                         erd: erd,
                         source: message.source,
@@ -81,17 +82,17 @@ exports.plugin = function (bus, configuration, callback) {
                     });
                 }
             }
-            
+
             delete reader;
         }
-        else if (message.command == COMMAND_ERD_WRITE) {        
+        else if (message.command == COMMAND_ERD_WRITE) {
             var reader = new stream.Reader(message.data, stream.BIG_ENDIAN);
             var count = reader.readUInt8();
 
             if (message.data.length == 1 + 2 * count) {
                 for (var i = 0; i < count; i++) {
                     var erd = reader.readUInt16();
-                    
+
                     bus.emit("write-response", {
                         erd: erd,
                         source: message.source,
@@ -117,18 +118,18 @@ exports.plugin = function (bus, configuration, callback) {
                             var writer = new stream.Writer(3, stream.BIG_ENDIAN);
                             writer.writeUInt8(1);
                             writer.writeUInt16(erd);
-                        
+
                             bus.send({
                                 command: COMMAND_ERD_WRITE,
                                 data: writer.toArray(),
                                 source: configuration.address,
                                 destination: message.source
                             });
-                            
+
                             delete writer;
                         }
                     }
-                    
+
                     bus.emit("write", {
                         erd: erd,
                         data: data,
@@ -137,13 +138,13 @@ exports.plugin = function (bus, configuration, callback) {
                     }, writeResponse);
                 }
             }
-            
+
             delete reader;
         }
         else if (message.command == COMMAND_ERD_SUBSCRIBE) {
             var reader = new stream.Reader(message.data, stream.BIG_ENDIAN);
             var count = reader.readUInt8();
-           
+
             if (message.data.length == 1) {
                 /* ignore subscribe response */
             }
@@ -151,7 +152,7 @@ exports.plugin = function (bus, configuration, callback) {
                 for (var i = 0; i < count; i++) {
                     var erd = reader.readUInt16();
                     var time = reader.readUInt8();
-           
+
                     bus.emit("subscribe", {
                         erd: erd,
                         source: message.source,
@@ -159,22 +160,22 @@ exports.plugin = function (bus, configuration, callback) {
                     });
                 }
             }
-            
+
             delete reader;
         }
         else if (message.command == COMMAND_ERD_PUBLISH) {
             var reader = new stream.Reader(message.data, stream.BIG_ENDIAN);
-           
+
             if (message.data.length == 0) {
                 /* ignore publish response */
             }
             else {
                 var count = reader.readUInt8();
-           
+
                 for (var i = 0; i < count; i++) {
                     var erd = reader.readUInt16();
                     var data = reader.readBytes(reader.readUInt8());
-           
+
                     bus.emit("publish", {
                         erd: erd,
                         data: data,
@@ -183,7 +184,14 @@ exports.plugin = function (bus, configuration, callback) {
                     });
                 }
             }
-            
+
+            bus.send({
+                command: COMMAND_ERD_PUBLISH_ACKNOWLEDGMENT,
+                data: null,
+                source: configuration.address,
+                destination: message.source
+            });
+
             delete reader;
         }
     });
@@ -192,66 +200,66 @@ exports.plugin = function (bus, configuration, callback) {
         var writer = new stream.Writer(3, stream.BIG_ENDIAN);
         writer.writeUInt8(1);
         writer.writeUInt16(erd.erd);
-        
+
         bus.send({
             command: COMMAND_ERD_READ,
             source: erd.source,
             destination: erd.destination,
             data: writer.toArray()
         });
-        
+
         delete writer;
     };
-    
+
     bus.write = function (erd) {
         var writer = new stream.Writer(4 + erd.data.length, stream.BIG_ENDIAN);
         writer.writeUInt8(1);
         writer.writeUInt16(erd.erd);
         writer.writeUInt8(erd.data.length);
         writer.writeBytes(erd.data);
-        
+
         bus.send({
             command: COMMAND_ERD_WRITE,
             source: erd.source,
             destination: erd.destination,
             data: writer.toArray()
         });
-        
+
         delete writer;
     };
-    
-    bus.publish = function (erd) {    
+
+    bus.publish = function (erd) {
         var writer = new stream.Writer(4 + erd.data.length, stream.BIG_ENDIAN);
         writer.writeUInt8(1);
         writer.writeUInt16(erd.erd);
         writer.writeUInt8(erd.data.length);
         writer.writeBytes(erd.data);
-    
+
         bus.send({
             command: COMMAND_ERD_PUBLISH,
             data: writer.toArray(),
             source: erd.source,
             destination: erd.destination
         });
-        
+
         delete writer;
     };
-    
+
     bus.subscribe = function (erd) {
         var writer = new stream.Writer(4, stream.BIG_ENDIAN);
         writer.writeUInt8(1);
         writer.writeUInt16(erd.erd);
         writer.writeUInt8(0);
-    
+
         bus.send({
             command: COMMAND_ERD_SUBSCRIBE,
             data: writer.toArray(),
             source: erd.source,
             destination: erd.destination
         }, callback);
-        
+
         delete writer;
     };
-    
+
     callback(bus);
 };
